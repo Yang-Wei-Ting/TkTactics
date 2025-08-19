e8901a59b0b5f09a664ab85cf2656e18b9e4db55
Wei-Ting Yang <williamatcg@gmail.com>
2025-08-19 12:52:05 +0800

    Feature: Adjust experience thresholds to make leveling more gradual.
    Signed-off-by: Wei-Ting Yang <williamatcg@gmail.com>

--- sources/game/soldiers/base.py
+++ sources/game/soldiers/base.py
@@ -158,7 +158,7 @@ class SoldierModel(GameObjectModel):
     def _learn(self, experience: int = 1) -> None:
         self.experience += experience
 
-        LEVEL_UP_EXPERIENCE_BY_LEVEL = {1: 4, 2: 8, 3: 16, 4: 32, 5: 65535}
+        LEVEL_UP_EXPERIENCE_BY_LEVEL = {1: 5, 2: 7, 3: 9, 4: 11, 5: 65535}
         while self.experience >= LEVEL_UP_EXPERIENCE_BY_LEVEL[self.level]:
             self.experience -= LEVEL_UP_EXPERIENCE_BY_LEVEL[self.level]
             self.level += 1
