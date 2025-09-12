caefbdc9c55b341a3ac5cf214cb19bd950f580f5
Wei-Ting Yang <williamatcg@gmail.com>
2025-09-12 12:55:46 +0800

    Feature: Replace tiles around building with low-mobility-cost terrain.
    Signed-off-by: Wei-Ting Yang <williamatcg@gmail.com>

--- sources/game/base.py
+++ sources/game/base.py
@@ -12,6 +12,7 @@ class GameObjectModel:
 
     # A cost of -1 consumes all mobility points.
     cost_by_coordinate: dict[tuple[int, int], int] = {}
+    image_id_by_coordinate: dict[tuple[int, int], int] = {}
     occupied_coordinates: set[tuple[int, int]] = set()
 
     def __init__(self, x: int, y: int) -> None:
